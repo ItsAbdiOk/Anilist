7f3447fa8b43394bf5e9c9c5110be6c07380f23f
Abdi <mo.abdirahman99@gmail.com>
2022-12-13 00:15:59 +0000

    Its takes multiple genres in but not 100% working

--- Anilist.py
+++ Anilist.py
@@ -1,50 +1,56 @@
+# Import the pandas library
+import pandas as pd
+# Import the JSON library
+import json
 #making requests to the anilist api
 import requests
-import json
+
 #anilist api url
 url = "https://graphql.anilist.co"
 
 #query to random manga from anilist that is romance, action and made after 2010
 query = """
-query ($page: Int , $genre: String ,$startDate_greater: FuzzyDateInt) {
+query ($page: Int , $genre_in: [String] ,$startDate_greater: FuzzyDateInt) {
     Page (page: $page, perPage: 1) {
-        media (genre: $genre,startDate_greater :$startDate_greater, type: MANGA) {
+        media (genre_in: $genre_in,startDate_greater :$startDate_greater, type: MANGA) {
             id
             title {
                 english
+                romaji
             }
-            coverImage {
-                large
-            }
-            description
             genres
-            startDate {
-                year
-                month
-                day
-            }
-            endDate {
-                year
-                month
-                day
-            }
-            chapters
-            volumes
-            status
-            format
-            siteUrl
         }
     }
 }
 """
-#variables for the query
+# First, get the user input and split it into a list of genres
+genre_string = input("Enter a list of genres, separated by commas: ")
+def UserInput(genre_string):
+    genre_list = genre_string.split(",")
+    # Next, make each genre have a capital first letter
+    capitalized_genre_list = []
+    for genre in genre_list:
+        capitalized_genre = genre[0].upper() + genre[1:]
+        capitalized_genre_list.append(capitalized_genre)
+    return capitalized_genre_list
+
+
+
+List = UserInput(genre_string)
 variables = {
-    "page": 1, "genre": "Romance" , "genre": "Action", "startDate_greater":  20160000
+    "page": 1, "genre_in": List , "startDate_greater":  20160000
 }
 response = requests.post(url, json={'query': query, 'variables': variables})
 
-#response in json format in a new file
-with open('anilist.json', 'w') as f:
-    json.dump(response.json(), f, indent=4)
+# parse the response text as JSON
+response_json = response.json()
 
+# get the media object from the response
+media = response_json['data']['Page']['media'][0]
 
+# get the romaji title from the media object
+title = media['title']['romaji']
+Genres = media['genres']
+# print the title
+print(title)
+print(Genres)
