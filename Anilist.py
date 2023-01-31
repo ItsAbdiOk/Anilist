8daa38bbd96aa23b9a87316027875aced10072d4
Abdi <mo.abdirahman99@gmail.com>
2023-01-31 19:05:13 +0000

    bruh

--- Anilist.py
+++ Anilist.py
@@ -1,8 +1,5 @@
-# Import the pandas library
 import pandas as pd
-# Import the JSON library
 import json
-#making requests to the anilist api
 import requests
 
 #anilist api url
@@ -10,9 +7,9 @@ url = "https://graphql.anilist.co"
 
 #query to random manga from anilist that is romance, action and made after 2010
 query = """
-query ($page: Int , $genre_in: [String] ,$startDate_greater: FuzzyDateInt) {
+query ($page: Int , $genre_in: [String] ,$startDate_greater: FuzzyDateInt, $sort: MediaSort , $popularity: Int) {
     Page (page: $page, perPage: 1) {
-        media (genre_in: $genre_in,startDate_greater :$startDate_greater, type: MANGA) {
+        media (genre_in: $genre_in,startDate_greater :$startDate_greater,sort:$sort, popularity :$popularity ,type: MANGA) {
             id
             title {
                 english
@@ -23,34 +20,38 @@ query ($page: Int , $genre_in: [String] ,$startDate_greater: FuzzyDateInt) {
     }
 }
 """
+
 # First, get the user input and split it into a list of genres
-genre_string = input("Enter a list of genres, separated by commas: ")
-def UserInput(genre_string):
+def UserInput():
+    genre_string = input("Enter a list of genres, separated by commas: ")
     genre_list = genre_string.split(",")
+    
     # Next, make each genre have a capital first letter
     capitalized_genre_list = []
     for genre in genre_list:
-        capitalized_genre = genre[0].upper() + genre[1:]
+        capitalized_genre = genre[0].upper() + genre[1:]    
         capitalized_genre_list.append(capitalized_genre)
+    # Validate the entered genres
+    # You can use the AniList API to get the list of available genres
+    # and check if the entered genres exist in that list
+    
     return capitalized_genre_list
 
 
-
-List = UserInput(genre_string)
+# Get the list of genres entered by the user
+List = ["Romance", "Action", "Comedy"]
 variables = {
-    "page": 1, "genre_in": List , "startDate_greater":  20160000
+    "page": 1, 
+    "genre_in": List ,
+    "sort": "popularity",
+    "startDate_greater":  20160000,
+    "type": "MANGA"
 }
+
+# Make the request to the AniList API
 response = requests.post(url, json={'query': query, 'variables': variables})
 
-# parse the response text as JSON
+# Parse the response text as JSON
 response_json = response.json()
 
-# get the media object from the response
-media = response_json['data']['Page']['media'][0]
-
-# get the romaji title from the media object
-title = media['title']['romaji']
-Genres = media['genres']
-# print the title
-print(title)
-print(Genres)
+print (response_json)
